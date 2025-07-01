f1b897667466658a187928a1d4b5f422f4aae8de
viraj <virajvelocity@gmail.com>
2025-07-02 00:38:38 +0530

    This is commit for Jenkins

--- src/test/java/Stepdefination/Searchpagestepdef.java
+++ src/test/java/Stepdefination/Searchpagestepdef.java
@@ -25,7 +25,7 @@ public class Searchpagestepdef
 	}
 
 	@Then("user should be able to see {string}, {string} and {string}")// MacBook  //MacBook Air  //MacBook Pro
-	public void user_should_be_able_to_see_and(String ExpectedProduct1, String ExpectedProduct2, String ExpectedProduct3) 
+	public void user_should_be_able_to_see_and(String ExpectedProduct1, String ExpectedProduct2, String ExpectedProduct3) throws InterruptedException 
 	{
 		ArrayList<String> Products=Sp.verifysearchedproduct();  //MacBook  ////MacBook Air  //MacBook Pro
 		
@@ -33,10 +33,11 @@ public class Searchpagestepdef
 		         String  ActualProduct2=Products.get(1);  //MacBook Air  
 		         String  ActualProduct3=Products.get(2);  //MacBook Pro
 		         
+		         Thread.sleep(2000);
 		Assert.assertEquals(ActualProduct1, ExpectedProduct1);
-		
+		 		Thread.sleep(2000);
 		Assert.assertEquals(ActualProduct2, ExpectedProduct2);       
-		  
+		 		Thread.sleep(2000);
 		Assert.assertEquals(ActualProduct3, ExpectedProduct3);   
 		
 	}
